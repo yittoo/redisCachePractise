b857c096c00560bb306d12498ae00b5f988157d0
yittoo <yittoo@users.noreply.github.com>
2019-05-11 20:32:36 +0300

    cache implemented

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://yittoo:1a23456789@ds153906.mlab.com:53906/advanced_node_sandbox',
   cookieKey: '123123123'
 };
